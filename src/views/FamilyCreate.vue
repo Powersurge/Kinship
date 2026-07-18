669432e7303494f4edcc1910bd2b0265da2142af
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 14:36:30 -0700

    Create FamilyCreate.vue

---
+++ src/views/FamilyCreate.vue
@@ -0,0 +1,56 @@
+<template>
+    <div>
+
+        <h2>
+            Create Family
+        </h2>
+
+        <FamilyForm
+            @submit="createFamily"
+        />
+
+    </div>
+</template>
+
+
+<script>
+
+import FamilyForm from '../components/FamilyForm.vue';
+
+export default {
+
+    name: 'FamilyCreate',
+
+    components: {
+        FamilyForm
+    },
+
+    methods: {
+
+        async createFamily(family) {
+
+            await fetch(
+                OC.generateUrl(
+                    '/apps/kinship/api/v1/families'
+                ),
+                {
+                    method: 'POST',
+
+                    headers: {
+                        'Content-Type':
+                            'application/json'
+                    },
+
+                    body: JSON.stringify(family)
+                }
+            );
+
+            this.$router.push('/families');
+
+        }
+
+    }
+
+}
+
+</script>
