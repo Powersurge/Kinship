edf51cead561d5e0088cc7c192375b2743305557
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 12:12:46 -0700

    Create Application.php

---
+++ appinfo/Application.php
@@ -0,0 +1,44 @@
+<?php
+
+declare(strict_types=1);
+
+namespace OCA\Kinship\AppInfo;
+
+use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootstrap;
+
+class Application extends App implements IBootstrap
+{
+    public const APP_ID = 'kinship';
+
+    public function __construct()
+    {
+        parent::__construct(self::APP_ID);
+    }
+
+    public function register(IRegistrationContext $context): void
+    {
+        // Service registrations will be added here.
+        //
+        // Example:
+        //
+        // $context->registerService(
+        //     PersonService::class,
+        //     function ($c) {
+        //         return new PersonService(
+        //             $c->get(PersonMapper::class)
+        //         );
+        //     }
+        // );
+    }
+
+    public function boot(IBootContext $context): void
+    {
+        // Runtime initialization will be added here.
+        //
+        // Examples:
+        // - Event listeners
+        // - Notification handlers
+        // - Extension registration
+    }
+}
