41837b4e7107e4bfcf15d0a6d67512e63869a2ab
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 14:08:15 -0700

    Create PersonCreate.vue

---
+++ src/views/PersonCreate.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+
+        <h2>
+            Add Person
+        </h2>
+
+        <PersonForm
+            @submit="createPerson"
+        />
+
+    </div>
+</template>
+
+
+<script>
+
+import PersonForm from '../components/PersonForm.vue';
+
+export default {
+
+    name: 'PersonCreate',
+
+    components: {
+        PersonForm
+    },
+
+    methods: {
+
+        async createPerson(person) {
+
+            await fetch(
+                OC.generateUrl(
+                    '/apps/kinship/api/v1/people'
+                ),
+                {
+                    method: 'POST',
+
+                    headers: {
+                        'Content-Type':
+                            'application/json'
+                    },
+
+                    body: JSON.stringify(person)
+                }
+            );
+
+            this.$router.push('/people');
+        }
+
+    }
+
+}
+
+</script>
