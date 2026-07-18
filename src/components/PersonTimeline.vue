63962f4cab5d2aa7b05fc243ae4ddb368819b2da
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 16:24:03 -0700

    Create PersonTimeline.vue

---
+++ src/components/PersonTimeline.vue
@@ -0,0 +1,123 @@
+<template>
+
+<div>
+
+<h3>
+Timeline
+</h3>
+
+
+<div
+v-for="event in events"
+:key="event.id"
+class="event"
+>
+
+<strong>
+{{ event.eventDate }}
+</strong>
+
+
+<p>
+{{ event.title }}
+</p>
+
+
+<small>
+{{ event.type }}
+</small>
+
+
+</div>
+
+
+</div>
+
+</template>
+
+
+<script>
+
+export default {
+
+
+name:'PersonTimeline',
+
+
+props:{
+
+personId:{
+type:Number,
+required:true
+}
+
+},
+
+
+data(){
+
+return {
+
+events:[]
+
+};
+
+},
+
+
+mounted(){
+
+this.loadEvents();
+
+},
+
+
+methods:{
+
+
+async loadEvents(){
+
+
+const response =
+await fetch(
+
+OC.generateUrl(
+
+'/apps/kinship/api/v1/timeline/person/'
++
+this.personId
+
+)
+
+);
+
+
+this.events =
+await response.json();
+
+
+}
+
+
+}
+
+
+}
+
+</script>
+
+
+<style scoped>
+
+.event {
+
+border-left:
+3px solid #0082c9;
+
+padding-left:15px;
+
+margin:15px;
+
+}
+
+</style>
