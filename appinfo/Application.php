70664f34bd4ef8f85eb75de0637e843f6c7c27b8
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 12:29:44 -0700

    Update Application.php

--- appinfo/Application.php
+++ appinfo/Application.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace OCA\Kinship\AppInfo;
 
+use OCA\Kinship\Db\PersonMapper;
+use OCA\Kinship\Service\PersonService;
 use OCP\AppFramework\App;
 use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
@@ -18,29 +20,31 @@ class Application extends App implements IBootstrap
         parent::__construct(self::APP_ID);
     }
 
-    public function register(IRegistrationContext $context): void
-    {
-        // Service registrations will be added here.
-        //
-        // Example:
-        //
-        // $context->registerService(
-        //     PersonService::class,
-        //     function ($c) {
-        //         return new PersonService(
-        //             $c->get(PersonMapper::class)
-        //         );
-        //     }
-        // );
+    public function register(
+        IRegistrationContext $context
+    ): void {
+        $context->registerService(
+            PersonMapper::class,
+            function ($c) {
+                return new PersonMapper(
+                    $c->get(\OCP\IDBConnection::class)
+                );
+            }
+        );
+
+        $context->registerService(
+            PersonService::class,
+            function ($c) {
+                return new PersonService(
+                    $c->get(PersonMapper::class)
+                );
+            }
+        );
     }
 
-    public function boot(IBootContext $context): void
-    {
+    public function boot(
+        IBootContext $context
+    ): void {
         // Runtime initialization will be added here.
-        //
-        // Examples:
-        // - Event listeners
-        // - Notification handlers
-        // - Extension registration
     }
 }
