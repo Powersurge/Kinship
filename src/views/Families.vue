d7ef1bd1078b6ad0636b2352f8114664e2d91e47
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 13:19:55 -0700

    Create Families.vue

---
+++ src/views/Families.vue
@@ -0,0 +1,44 @@
+<template>
+    <div>
+        <h2>Families</h2>
+
+        <ul>
+            <li
+                v-for="family in families"
+                :key="family.id"
+            >
+                {{ family.name }}
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Families',
+
+    data() {
+        return {
+            families: []
+        };
+    },
+
+    mounted() {
+        this.loadFamilies();
+    },
+
+    methods: {
+        async loadFamilies() {
+            const response = await fetch(
+                OC.generateUrl(
+                    '/apps/kinship/api/v1/families'
+                )
+            );
+
+            const data = await response.json();
+
+            this.families = data.families;
+        }
+    }
+}
+</script>
