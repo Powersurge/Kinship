a2ba036ce2fbbba13176f928a4101130097cf9f0
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 13:24:36 -0700

    Create TimelineItem.vue

---
+++ src/components/TimelineItem.vue
@@ -0,0 +1,36 @@
+<template>
+    <div class="timeline-item">
+        <strong>
+            {{ event.eventDate }}
+        </strong>
+
+        <div>
+            {{ event.title }}
+        </div>
+
+        <small>
+            {{ event.type }}
+        </small>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TimelineItem',
+
+    props: {
+        event: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.timeline-item {
+    border-left: 3px solid #0082c9;
+    padding-left: 15px;
+    margin: 15px 0;
+}
+</style>
