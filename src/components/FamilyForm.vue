9632c31f52c188ee3cb3ad1f4f691f9c6a82e657
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 14:35:46 -0700

    Create FamilyForm.vue

---
+++ src/components/FamilyForm.vue
@@ -0,0 +1,82 @@
+<template>
+    <form @submit.prevent="submit">
+
+        <label>
+            Family Name
+            <input
+                v-model="form.name"
+                required
+            />
+        </label>
+
+        <label>
+            Description
+            <textarea
+                v-model="form.description"
+            />
+        </label>
+
+        <button type="submit">
+            Save Family
+        </button>
+
+    </form>
+</template>
+
+<script>
+export default {
+
+    name: 'FamilyForm',
+
+    props: {
+        value: {
+            type: Object,
+            default: () => ({})
+        }
+    },
+
+    data() {
+        return {
+            form: {
+                name: '',
+                description: ''
+            }
+        };
+    },
+
+    mounted() {
+        Object.assign(
+            this.form,
+            this.value
+        );
+    },
+
+    methods: {
+
+        submit() {
+            this.$emit(
+                'submit',
+                this.form
+            );
+        }
+
+    }
+
+}
+
+</script>
+
+<style scoped>
+
+form {
+    display: flex;
+    flex-direction: column;
+    gap: 12px;
+    max-width: 400px;
+}
+
+textarea {
+    min-height: 100px;
+}
+
+</style>
