b2d16465e3408f145b1fc568edc3fcc4b42088ab
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 16:21:28 -0700

    Create EventCreate.vue

---
+++ src/views/EventCreate.vue
@@ -0,0 +1,80 @@
+<template>
+
+<div>
+
+<h2>
+Create Event
+</h2>
+
+
+<EventForm
+    @submit="createEvent"
+/>
+
+
+</div>
+
+</template>
+
+
+<script>
+
+import EventForm
+from '../components/EventForm.vue';
+
+
+export default {
+
+
+name:'EventCreate',
+
+
+components:{
+
+EventForm
+
+},
+
+
+methods:{
+
+
+async createEvent(event){
+
+
+await fetch(
+
+OC.generateUrl(
+'/apps/kinship/api/v1/events'
+),
+
+{
+
+method:'POST',
+
+headers:{
+
+'Content-Type':
+'application/json'
+
+},
+
+body:JSON.stringify(event)
+
+}
+
+);
+
+
+this.$router.push('/events');
+
+
+}
+
+
+}
+
+
+}
+
+</script>
