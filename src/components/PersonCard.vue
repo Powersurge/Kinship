f7ecea2497322e33d85829ba8529ca9b503fd9c7
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 13:22:00 -0700

    Create PersonCard.vue

---
+++ src/components/PersonCard.vue
@@ -0,0 +1,42 @@
+<template>
+    <div class="card">
+        <h3>
+            {{ person.firstName }}
+            {{ person.lastName }}
+        </h3>
+
+        <p v-if="person.birthDate">
+            Born: {{ person.birthDate }}
+        </p>
+
+        <p v-if="person.alive">
+            Living
+        </p>
+
+        <p v-else>
+            Deceased
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'PersonCard',
+
+    props: {
+        person: {
+            type: Object,
+            required: true
+        }
+    }
+}
+</script>
+
+<style scoped>
+.card {
+    border: 1px solid #ddd;
+    border-radius: 8px;
+    padding: 12px;
+    margin: 8px;
+}
+</style>
