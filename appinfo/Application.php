7267dec2605b57eb973fbdeee1ef578cf1d1ee73
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 12:15:16 -0700

    Update Application.php

--- appinfo/Application.php
+++ appinfo/Application.php
@@ -5,7 +5,9 @@ declare(strict_types=1);
 namespace OCA\Kinship\AppInfo;
 
 use OCP\AppFramework\App;
+use OCP\AppFramework\Bootstrap\IBootContext;
 use OCP\AppFramework\Bootstrap\IBootstrap;
+use OCP\AppFramework\Bootstrap\IRegistrationContext;
 
 class Application extends App implements IBootstrap
 {
