dd293a9e003f656f70f318d837d88c1701281338
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 13:16:18 -0700

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+    <div id="kinship">
+        <h1>Kinship</h1>
+
+        <nav>
+            <router-link to="/">Dashboard</router-link>
+            |
+            <router-link to="/people">People</router-link>
+            |
+            <router-link to="/families">Families</router-link>
+            |
+            <router-link to="/events">Events</router-link>
+        </nav>
+
+        <router-view />
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'App'
+}
+</script>
+
+<style scoped>
+#kinship {
+    padding: 20px;
+}
+
+nav {
+    margin-bottom: 20px;
+}
+</style>
