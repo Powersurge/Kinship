97257675fb3de04aaa800409039be53a8c54680d
Powersurge <Powersurge@users.noreply.github.com>
2026-07-18 13:18:51 -0700

    Create Dashboard.vue

---
+++ src/views/Dashboard.vue
@@ -0,0 +1,21 @@
+<template>
+    <div>
+        <h2>Kinship Dashboard</h2>
+
+        <p>
+            Welcome to your family history workspace.
+        </p>
+
+        <ul>
+            <li>Manage family members</li>
+            <li>Build relationships</li>
+            <li>Track family events</li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Dashboard'
+}
+</script>
